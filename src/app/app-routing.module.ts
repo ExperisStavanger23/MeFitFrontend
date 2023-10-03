eee627addc3eed3581fa7a5dac770b6bd2bb20c1
andyret26 <andyret26@gmail.com>
2023-10-03 15:03:26 +0200

    added Onboarding page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,14 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { NgModule } from "@angular/core"
+import { RouterModule, Routes } from "@angular/router"
+import { OnboardingPage } from "./pages/onboarding/onboarding.page"
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "onboarding", component: OnboardingPage },
+  { path: "path2", component: OnboardingPage },
+]
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
---
+++ src/app/pages/onboarding/onboarding.page.ts
@@ -0,0 +1,35 @@
+import { Component } from "@angular/core"
+import { FormBuilder, FormGroup, Validators } from "@angular/forms"
+
+@Component({
+  selector: "app-onboarding",
+  templateUrl: "./onboarding.page.html",
+  styleUrls: ["./onboarding.page.css"],
+})
+export class OnboardingPage {
+  form: FormGroup
+  minDate = new Date(1950, 1, 1)
+  maxDate = new Date().getFullYear()
+
+  constructor(private fb: FormBuilder) {
+    this.form = fb.group({
+      name: ["", Validators.required],
+      email: ["", [Validators.required, Validators.email]],
+      experience: [null],
+      gender: [null],
+      height: [null, Validators.pattern("^([5-9][0-9]|[1-2][0-9]{2}|300)$")],
+      weight: [null, Validators.pattern("^(1[0-9]{1,2}|[2-6][0-9]{1,2}|700)$")],
+      bDay: [null],
+    })
+  }
+
+  handleSubmit(event: SubmitEvent): void {
+    event.preventDefault()
+    console.log(this.form.value)
+  }
+}
+
+export interface UserModel {
+  name: string
+  email: string
+}
