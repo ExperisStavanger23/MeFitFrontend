24f5b3bfccb558e0be39a87264c9d50ac0e75681
andyret26 <andyret26@gmail.com>
2023-10-03 10:53:39 +0200

    Keycloak setup + eslint/prettier

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,39 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { APP_INITIALIZER, NgModule } from "@angular/core"
+import { BrowserModule } from "@angular/platform-browser"
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppRoutingModule } from "./app-routing.module"
+import { AppComponent } from "./app.component"
+
+import { KeycloakAngularModule, KeycloakService } from "keycloak-angular"
+import { environment } from "src/environment/environment"
+console.log(environment.keycloakUrl)
+function initializeKeycloak(keycloak: KeycloakService) {
+  return () =>
+    keycloak.init({
+      config: {
+        url: environment.keycloakUrl,
+        realm: environment.keycloakRealm,
+        clientId: environment.keycloakClientId,
+      },
+      initOptions: {
+        onLoad: "check-sso",
+        silentCheckSsoRedirectUri:
+          window.location.origin + "/assets/silent-check-sso.html",
+      },
+    })
+}
 
 @NgModule({
-  declarations: [
-    AppComponent
+  declarations: [AppComponent],
+  imports: [BrowserModule, AppRoutingModule, KeycloakAngularModule],
+  providers: [
+    {
+      provide: APP_INITIALIZER,
+      useFactory: initializeKeycloak,
+      multi: true,
+      deps: [KeycloakService],
+    },
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
