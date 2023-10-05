720be10474500fee9a357e32ec42223041270619
Sjur G <sjur.gustavsen@no.experis.com>
2023-10-05 13:34:52 +0200

    select multiple and objects

--- src/app/components/program-creation-form/program-creation-form.component.ts
+++ src/app/components/program-creation-form/program-creation-form.component.ts
@@ -1,6 +1,23 @@
 import { Component } from "@angular/core"
 import { FormBuilder, FormGroup, Validators } from "@angular/forms"
 
+type workout = {
+  id: number
+  name: string
+  duration: number
+  description: string
+  imageUrl: string
+  exercises: exercise[]
+}
+type exercise = {
+  id: number
+  name: string
+  reps: number
+  description: string
+  imageUrl: string
+  tags: string[]
+}
+
 @Component({
   selector: "app-program-creation-form",
   templateUrl: "./program-creation-form.component.html",
@@ -8,8 +25,32 @@ import { FormBuilder, FormGroup, Validators } from "@angular/forms"
 })
 export class ProgramCreationFormComponent {
   form: FormGroup
-  minDate = new Date(1950, 1, 1)
-  maxDate = new Date().getFullYear()
+
+  //TODO: replace with data from database/api
+  experienceLevels = ["Beginner", "Intermediate", "Advanced"]
+  workoutCategories = ["Strength", "Cardio", "Yoga"]
+
+  workout: workout = {
+    id: 1,
+    name: "Big Chest workout",
+    duration: 1,
+    description: "a set of chest exercises",
+    imageUrl:
+      "https://hips.hearstapps.com/hmg-prod/images/muscular-man-doing-push-ups-during-home-workout-royalty-free-image-1678105289.jpg?crop=0.668xw:1.00xh;0.106xw,0&resize=1200:*",
+    exercises: [
+      {
+        id: 1,
+        name: "pushups",
+        reps: 10,
+        description: "pushups",
+        imageUrl:
+          "https://hips.hearstapps.com/hmg-prod/images/muscular-man-doing-push-ups-during-home-workout-royalty-free-image-1678105289.jpg?crop=0.668xw:1.00xh;0.106xw,0&resize=1200:*",
+        tags: ["chest", "arms"],
+      },
+    ],
+  }
+
+  workouts: workout[] = [this.workout]
 
   constructor(private fb: FormBuilder) {
     this.form = fb.group({
