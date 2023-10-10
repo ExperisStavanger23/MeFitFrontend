07eddee38b3c425c48d77d0acc276dabe4fdc5a3
Sjur G <sjur.gustavsen@no.experis.com>
2023-10-10 11:17:56 +0200

    program details

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,16 +6,16 @@ import { WorkoutsPage } from "./pages/workouts/workouts.page"
 import { ExercisesPage } from "./pages/exercises/exercises.page"
 import { ProfilePage } from "./pages/profile/profile.page"
 import { DashboardPage } from "./pages/dashboard/dashboard.page"
-import { DetailsCardComponent } from "./components/details-card/details-card.component"
 import { CreationPage } from "./pages/creation/creation.page"
 import { ExerciseDetailsCardComponent } from "./components/exercise-components/exercise-details-card/exercise-details-card.component"
 import { WorkoutDetailsCardComponent } from "./components/workout-components/workout-details-card/workout-details-card.component"
+import { ProgramDetailsCardComponent } from "./components/program-components/program-details-card/program-details-card.component"
 
 const routes: Routes = [
   { path: "onboarding", component: OnboardingPage },
   { path: "dashboard", component: DashboardPage },
   { path: "programs", component: ProgramsPage },
-  { path: "programs/:id", component: DetailsCardComponent },
+  { path: "programs/:id", component: ProgramDetailsCardComponent },
   { path: "workouts", component: WorkoutsPage },
   { path: "workouts/:id", component: WorkoutDetailsCardComponent },
   { path: "exercises", component: ExercisesPage },
