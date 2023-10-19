e37628bc152ae2ccb222cf273676b6c9ef860107
Sjur G <sjur.gustavsen@no.experis.com>
2023-10-19 13:52:25 +0200

    Workout service comments

--- src/app/services/api-workout.service.ts
+++ src/app/services/api-workout.service.ts
@@ -8,18 +8,35 @@ import { catchError, map } from "rxjs/operators"
 @Injectable({
   providedIn: "root",
 })
+/**
+ * Class responsible for managing workout-related API interactions.
+ */
 export class ApiWorkoutService {
   apiUrlBase = "http://localhost:5212/api/v1"
   constructor(private http: HttpClient) {}
 
+  /**
+   * Gets all the workouts.
+   * @returns An observable of all workouts.
+   */
   getAllWorkouts(): Observable<Workout[]> {
     return this.http.get<Workout[]>(`${this.apiUrlBase}/Workout`)
   }
 
+  /**
+   * Gets a workout by its id.
+   * @param id - The id of the workout to get.
+   * @returns - The workout as an observable.
+   */
   getWorkoutById(id: number): Observable<WorkoutGet> {
     return this.http.get<WorkoutGet>(`${this.apiUrlBase}/Workout/${id}`)
   }
 
+  /**
+   * Create a new workout.
+   * @param workout - The workout to be created.
+   * @returns - A boolean indicating whether the workout was successfully created.
+   */
   postWorkout(workout: PostWorkout): Observable<boolean> {
     const body = JSON.stringify(workout)
 
