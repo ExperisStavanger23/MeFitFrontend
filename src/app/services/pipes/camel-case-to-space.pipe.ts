903415cf46dab36e5796ef1f3bcfe022193e61cc
Sjur G <sjur.gustavsen@no.experis.com>
2023-10-19 13:56:06 +0200

    Pipes comments and folder structure

--- src/app/services/pipes/camel-case-to-space.pipe.ts
+++ src/app/services/pipes/camel-case-to-space.pipe.ts
@@ -3,6 +3,9 @@ import { Pipe, PipeTransform } from "@angular/core"
 @Pipe({
   name: "camelCaseToSpace",
 })
+/**
+ * Pipe that converts camel case to words with spaces.
+ */
 export class CamelCaseToSpacePipe implements PipeTransform {
   transform(value: string): string {
     // Use regular expressions to insert spaces
