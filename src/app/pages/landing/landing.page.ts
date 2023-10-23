9efcb19e88f3e3a4a49ab9fdcc5ffcc41956dd52
Sjur G <sjur.gustavsen@no.experis.com>
2023-10-23 09:25:33 +0200

    buttons links to correct pages

--- src/app/pages/landing/landing.page.ts
+++ src/app/pages/landing/landing.page.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core"
+import { Router } from "@angular/router"
+import { KeycloakService } from "keycloak-angular"
 
 @Component({
-  selector: 'app-landing',
-  templateUrl: './landing.page.html',
-  styleUrls: ['./landing.page.css']
+  selector: "app-landing",
+  templateUrl: "./landing.page.html",
+  styleUrls: ["./landing.page.css"],
 })
 export class LandingPage {
+  constructor(
+    private router: Router,
+    private readonly keycloak: KeycloakService
+  ) {}
 
+  navigateToPrograms(): void {
+    this.router.navigate(["/programs"])
+  }
+
+  navigateToUser(): void {
+    this.keycloak.login({
+      redirectUri: window.location.origin + "/onboarding",
+    })
+  }
+  navigateToWorkouts(): void {
+    this.router.navigate(["/workouts"])
+  }
+
+  navigateToExercises(): void {
+    this.router.navigate(["/exercises"])
+  }
 }
